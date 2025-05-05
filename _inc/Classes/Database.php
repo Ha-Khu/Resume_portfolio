d51b6492a74f0deebd946c5dfef24084bb5306ef
Ha-Khu <davidplevka@gmail.com>
2025-05-05 23:31:56 +0200

    Premiestnenie Databse.php do Classes

---
+++ _inc/Classes/Database.php
@@ -0,0 +1,28 @@
+<?php 
+class Database {
+    public function __construct() {
+        $host = 'localhost';
+        $db = 'portfolio_db';
+        $user = 'root';
+        $pass = '';
+        $charset = 'utf8mb4';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ];
+
+        try {
+            $this->pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public function getPdo() {
+        return $this->pdo;
+    }
+
+}
+?>
